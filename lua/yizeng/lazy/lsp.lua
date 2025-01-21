1c7ea3c5ee2173e4de04a9ae5c01619c031b33fe
Yi Zeng <calvin.zengyi@gmail.com>
2025-01-20 17:33:37 -0800

    some lsp configs

--- lua/yizeng/lazy/lsp.lua
+++ lua/yizeng/lazy/lsp.lua
@@ -94,7 +94,7 @@ return{
                        }
                    }
                }
-           end
+           end,
         }
       })
     end
--- lua/yizeng/set.lua
+++ lua/yizeng/set.lua
@@ -1,5 +1,7 @@
 vim.opt.guicursor = ""
 vim.opt.nu = true
+vim.opt.encoding=utf8
+vim.opt.fileencoding=utf8
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -17,4 +19,33 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
+-- diagnostic settings
+vim.diagnostic.config({
+  virtual_text = false, -- Turn off inline diagnostics
+})
 
+-- Use this if you want it to automatically show all diagnostics on the
+-- current line in a floating window. Personally, I find this a bit
+-- distracting and prefer to manually trigger it (see below). The
+-- CursorHold event happens when after `updatetime` milliseconds. The
+-- default is 4000 which is much too long
+vim.cmd('autocmd CursorHold * lua vim.diagnostic.open_float()')
+vim.o.updatetime = 300
+
+-- Show all diagnostics on current line in floating window
+vim.api.nvim_set_keymap(
+  'n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>',
+  { noremap = true, silent = true }
+)
+-- Go to next diagnostic (if there are multiple on the same line, only shows
+-- one at a time in the floating window)
+vim.api.nvim_set_keymap(
+  'n', '<Leader>n', ':lua vim.diagnostic.goto_next()<CR>',
+  { noremap = true, silent = true }
+)
+-- Go to prev diagnostic (if there are multiple on the same line, only shows
+-- one at a time in the floating window)
+vim.api.nvim_set_keymap(
+  'n', '<Leader>p', ':lua vim.diagnostic.goto_prev()<CR>',
+  { noremap = true, silent = true }
+)
